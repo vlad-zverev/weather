248128ad0dbe4486dc0513c1b116342a205e01f0
vzverev <vzverev@freetechs.org>
2023-03-24 16:42:32 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Weather Bot
+
+Requirements:
+``` 
+Python 3.9+
+```
+Run:
+
+```angular2html
+sh runner.sh
+```
---
+++ runner.sh
@@ -0,0 +1,6 @@
+source secrets.env
+export WEATHER_API_KEY
+export TELEGRAM_TOKEN
+export LOG_LEVEL=INFO
+pip3 install -r requirements.txt
+python3 main.py
