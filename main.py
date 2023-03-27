ebee3be3d2914e50d8ed593be040ce1940a42dd6
vzverev <vzverev@freetechs.org>
2023-03-27 04:41:10 +0400

    flake

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import logging
 from os import getenv
+from sys import stdout
 
 from bot import Bot
 
-logging.basicConfig(level=getenv('LOG_LEVEL'), format='%(asctime)s %(levelname)s (%(levelno)s) %(message)s')
+logging.basicConfig(
+    level=getenv('LOG_LEVEL', 'INFO'),
+    format='%(asctime)s %(levelname)s (%(levelno)s) %(message)s',
+    stream=stdout,
+)
 
 bot = Bot()
 if __name__ == '__main__':
--- runner.sh
+++ runner.sh
@@ -3,4 +3,4 @@ export WEATHER_API_KEY
 export TELEGRAM_TOKEN
 export LOG_LEVEL=INFO
 pip3 install -r requirements.txt
-python3 main.py
+python3.11 main.py
